366ed693fa385f38627aee17a072538dfac1a98a
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 16:59:55 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkaproducerconsumerupstash/KafkaProducerConsumerUpstashApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafkaproducerconsumerupstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerUpstashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerConsumerUpstashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/kafkaproducerconsumerupstash/KafkaProducerConsumerUpstashApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.kafkaproducerconsumerupstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerConsumerUpstashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
